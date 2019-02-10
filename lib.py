a87b225c4e872e6fbdc3c097eff0a9d4facdae23
student <student@localhost>
2019-02-10 09:29:45 +0300

    refactoring: упростили структуру кода (убрали вложенность)

--- lib.py
+++ lib.py
@@ -1,18 +1,16 @@
 def is_creditable(age, salary):
     """
-    >>>is_creditable(30, 40_000)
+    >>> is_creditable(30, 40_000)
     True
 
-    >>>is_creditable(20, 40_000)
+    >>> is_creditable(20, 40_000)
     False
 
-    >>>is_creditable(70, 40_000)
+    >>> is_creditable(70, 40_000)
     False
 
-    >>>is_creditable(30, 10_000)
+    >>> is_creditable(30, 10_000)
     False
-
-
     """
 
 
@@ -20,13 +18,7 @@ def is_creditable(age, salary):
     max_age = 60
     min_salary = 30_000
 
-    if age >= min_age:
-        if age <= max_age:
-            if salary >= min_salary:
-                return True
-            else:
-                return False
-        else:
-             return False
+    if age >= min_age and age <= max_age and salary >= min_salary:
+        return True
     else:
         return False
